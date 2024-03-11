a4ffd65214e913dba30e0a32b72f082c9498c235
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2024-03-11 15:53:04 -0400

    Add project and save project to local storage

--- src/Project.js
+++ src/Project.js
@@ -2,4 +2,8 @@ export default function Project(title) {
   this.id = Date.now().toString()
   this.title = title
   this.todos = []
+
+  this.addTodo = function (todo) {
+    this.todos.push(todo)
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,64 +1,48 @@
 import Project from './Project.js'
 import Todo from './Todo.js'
 
-const LOCAL_STORAGE_PROJECT_KEY = 'todo.myProjects'
-// const LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY = 'todo.selectedProjectId'
-let myProjects =
-  JSON.parse(localStorage.getItem(LOCAL_STORAGE_PROJECT_KEY)) || []
-// let selectedProjectId = localStorage.getItem(
-//   LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY
-// )
-
-const projectsContainer = document.getElementById('projects')
-const todosContainer = document.getElementById('todos')
-const inputTodo = document.getElementById('inputTodo')
 const addProject = document.getElementById('addProject')
+const addProjectForm = document.getElementById('addProjectForm')
+const projectContainer = document.getElementById('projects')
+const todoForm = document.getElementById('todoForm')
 
-function displayProjects() {
-  projectsContainer.innerHTML = ''
-  myProjects.forEach((project, index) => {
+const LOCAL_STORAGE_PROJECT_KEY = 'todo.projects'
+let projects = JSON.parse(localStorage.getItem(LOCAL_STORAGE_PROJECT_KEY)) || []
+
+function render() {
+  clearElement(projectContainer)
+  projects.forEach((project, index) => {
     let projectElement = document.createElement('li')
+    projectElement.dataset.projectId = project.id
     projectElement.classList.add('list')
     projectElement.textContent = project.title
-    projectElement.addEventListener('click', () => {
-      let inputTodoElement = document.createElement('input')
-      inputTodoElement.setAttribute('id', 'inputTodo')
-      todosContainer.appendChild(inputTodoElement)
-    })
-    let deleteButton = document.createElement('button')
-    deleteButton.textContent = 'delete'
-    deleteButton.addEventListener('click', () => {
-      deleteProject(index)
-    })
-    projectElement.appendChild(deleteButton)
-    projectsContainer.appendChild(projectElement)
+    projectContainer.appendChild(projectElement)
   })
 }
 
-function deleteProject(index) {
-  myProjects.splice(index, 1)
-  displayProjects()
+function clearElement(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild)
+  }
 }
 
-addProject.addEventListener('change', (e) => {
+function saveAndRender() {
+  save()
+  render()
+}
+
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_PROJECT_KEY, JSON.stringify(projects))
+}
+
+addProjectForm.addEventListener('change', (e) => {
+  e.preventDefault()
   let title = e.target.value
+  if (title == null || title === '') return
   const newProject = new Project(title)
-  myProjects.push(newProject)
+  projects.push(newProject)
   e.target.value = null
-  displayProjects()
+  saveAndRender()
 })
-if (inputTodo) {
-  inputTodo.addEventListener('change', () => {
-    console.log('test')
-  })
-}
 
-// function displayTodos(projectIndex) {
-//   todosContainer.innerHTML = ''
-//   const selectedProject = myProjects[projectIndex]
-//   selectedProject.todos.forEach((todo) => {
-//     let todoElement = document.createElement('li')
-//     todoElement.textContent = todo.title
-//     todosContainer.appendChild(todoElement)
-//   })
-// }
+render()
